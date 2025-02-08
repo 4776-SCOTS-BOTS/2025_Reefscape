95cc979d86adda6b82b73f700f8528ab51f1c969
Matthew Bannick <mbannick6@gmail.com>
2025-02-08 11:28:18 -0800

    made the intake stop when it's holdingh somthing

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,18 +16,21 @@ public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
 
   public SparkMax intakeMotor, wristMotor;
+  LinearFilter filter = LinearFilter.movingAverage(4);
   
   public Intake() {
 
     intakeMotor = new SparkMax(Constants.IntakeConstants.intakeMotorCANID, MotorType.kBrushless);
     wristMotor = new SparkMax(Constants.IntakeConstants.wristMotorCANID, MotorType.kBrushless);
 
-
-    
   }
 
   @Override
   public void periodic(){
+    if(filter.calculate(intakeMotor.getOutputCurrent()) > 1){ //! number is temporary
+      Timer.delay(0.5);
+      intakeOff();
+    }
     // This method will be called once per scheduler run
   }
 
